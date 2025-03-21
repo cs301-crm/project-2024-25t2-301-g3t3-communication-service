594d4ab0d11223a3f27a6f32f0667eb99d4c42d6
FQTeo <fuqiangteo@gmail.com>
2025-03-22 00:03:46 +0800

    refactor: remove unnecessary methods

--- src/main/java/com/cs301/communication_service/mappers/CommunicationMapper.java
+++ src/main/java/com/cs301/communication_service/mappers/CommunicationMapper.java
@@ -25,46 +25,6 @@ public class CommunicationMapper {
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
-    /**
-     * Converts Communication model to CommunicationDTOResponse
-     */
-    public CommunicationDTOResponse toDto(Communication model) {
-        if (model == null) return null;
-
-        CommunicationDTOResponse dto = CommunicationDTOResponse.builder()
-                .communicationId(model.getId())
-                .agentId(model.getAgentId())
-                .clientId(model.getClientId())
-                .clientEmail(model.getClientEmail())
-                .crudType(model.getCrudType())
-                .subject(model.getSubject())
-                // .messageBody(model.getMessageBody())
-                .status(model.getStatus())
-                .timestamp(model.getTimestamp().format(DATE_FORMATTER))
-                .build();
-
-        return dto;
-    }
-
-    /**
-     * Converts CommunicationDTO to Communication model
-     */
-    public Communication toModel(CommunicationDTO dto) {
-        if (dto == null) return null;
-
-        Communication model = new Communication();
-        model.setAgentId(dto.getAgentId());
-        model.setClientId(dto.getClientId());
-        model.setClientEmail(dto.getClientEmail());
-        model.setCrudType(dto.getCrudType());
-        model.setSubject(getSubjectFromCrudType(dto.getCrudType()));
-        // model.setMessageBody(dto.getMessageBody());
-        model.setStatus(CommunicationStatus.SENT);
-        // model.setTimestamp(LocalDateTime.parse(dto.getTimestamp(), DATE_FORMATTER));
-
-        return model;
-    }
-
     public OtpCommunication otpToModel(ConsumerRecord<String, Otp> record) {
         Otp otpMessage = record.value();
 

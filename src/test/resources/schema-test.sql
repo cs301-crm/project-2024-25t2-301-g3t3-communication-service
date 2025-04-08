b5ccc31e6f4c76b33c9890c8f30f5e0b4f827366
FQTeo <fuqiangteo@gmail.com>
2025-04-08 08:55:42 +0800

    fix: make sql h2 compatible

--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS otp_communications;
 DROP TABLE IF EXISTS account_communications;
 
 CREATE TABLE IF NOT EXISTS client_communications (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id UUID DEFAULT random_uuid() PRIMARY KEY,
     agent_id VARCHAR(255) NOT NULL,
     client_id VARCHAR(255) NOT NULL,
-    client_email VARCHAR(255) NOT NULL CHECK (client_email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    client_email VARCHAR(255) NOT NULL CHECK (client_email REGEXP '(?i)^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'),
     crud_type VARCHAR(50) NOT NULL,
     subject VARCHAR(255) NOT NULL,
     status VARCHAR(50) NOT NULL,
@@ -15,10 +15,10 @@ CREATE TABLE IF NOT EXISTS client_communications (
 );
 
 CREATE TABLE IF NOT EXISTS account_communications (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id UUID DEFAULT random_uuid() PRIMARY KEY,
     agent_id VARCHAR(255) NOT NULL,
     client_id VARCHAR(255) NOT NULL,
-    client_email VARCHAR(255) NOT NULL CHECK (client_email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    client_email VARCHAR(255) NOT NULL CHECK (client_email REGEXP '(?i)^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'),
     account_id VARCHAR(255) NOT NULL,
     account_type VARCHAR(255) NOT NULL,
     crud_type VARCHAR(50) NOT NULL,
@@ -28,10 +28,10 @@ CREATE TABLE IF NOT EXISTS account_communications (
 );
 
 CREATE TABLE IF NOT EXISTS user_communications (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id UUID DEFAULT random_uuid() PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     user_role VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL CHECK (user_email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    user_email VARCHAR(255) NOT NULL CHECK (user_email REGEXP '(?i)^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'),
     temp_password VARCHAR(255) NOT NULL,
     subject VARCHAR(255) NOT NULL,
     status VARCHAR(50) NOT NULL,
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS user_communications (
 );
 
 CREATE TABLE IF NOT EXISTS otp_communications (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    email VARCHAR(255) NOT NULL CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    id UUID DEFAULT random_uuid() PRIMARY KEY,
+    email VARCHAR(255) NOT NULL CHECK (email REGEXP '(?i)^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'),
     otp INT NOT NULL,
     subject VARCHAR(255) NOT NULL,
     status VARCHAR(50) NOT NULL,

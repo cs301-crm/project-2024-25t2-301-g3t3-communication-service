c2a0886fd47ccf15d8e5df2fc6f3f30ea93fe2c9
FQTeo <fuqiangteo@gmail.com>
2025-04-08 09:11:24 +0800

    fix: client_email check

--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS client_communications (
     id UUID DEFAULT random_uuid() PRIMARY KEY,
     agent_id VARCHAR(255) NOT NULL,
     client_id VARCHAR(255) NOT NULL,
-    client_email VARCHAR(255) NOT NULL CHECK (client_email REGEXP '(?i)^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'),
+    client_email VARCHAR(255) NOT NULL CHECK (client_email REGEXP '(?i)^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$'),
     crud_type VARCHAR(50) NOT NULL,
     subject VARCHAR(255) NOT NULL,
     status VARCHAR(50) NOT NULL,

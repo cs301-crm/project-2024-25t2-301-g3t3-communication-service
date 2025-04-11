3ee79a66823497d4f6b20a43e1a86814d18aded7
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-12 00:55:43 +0800

    feat: prod config

--- src/main/java/com/cs301/communication_service/configs/KafkaUserConsumerConfig.java
+++ src/main/java/com/cs301/communication_service/configs/KafkaUserConsumerConfig.java
@@ -1,5 +1,7 @@
 package com.cs301.communication_service.configs;
 
+import com.amazonaws.services.schemaregistry.deserializers.GlueSchemaRegistryKafkaDeserializer;
+import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 // import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -15,6 +17,8 @@ import com.cs301.shared.protobuf.U2C;
 
 // import software.amazon.glue.schema.registry.serializers.GlueSchemaRegistryKafkaDeserializer;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.glue.model.DataFormat;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +31,9 @@ public class KafkaUserConsumerConfig {
 
     @Value("${kafka.schema.registry}")
     private String schemaRegistryUrl;
+
+    @Value("${kafka.schema.protobuf.u2c}")
+    private String protobufU2CSchema;
     
     @Bean
     public ConsumerFactory<String, String> userConsumerFactory() {
@@ -38,10 +45,13 @@ public class KafkaUserConsumerConfig {
 
         // Deserializers
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GlueSchemaRegistryKafkaDeserializer.class.getName());
+        props.put(AWSSchemaRegistryConstants.AWS_REGION, Region.AP_SOUTHEAST_1);
+        props.put(AWSSchemaRegistryConstants.DATA_FORMAT, DataFormat.PROTOBUF.name());
+        props.put(AWSSchemaRegistryConstants.REGISTRY_NAME, schemaRegistryUrl);
+        props.put(AWSSchemaRegistryConstants.SCHEMA_NAME, protobufU2CSchema);
 
         // Schema Registry URL configuration
-        props.put("schema.registry.url", schemaRegistryUrl);
         props.put("specific.protobuf.value.type", U2C.class.getName());
         
         // Set missing topics to non-fatal
